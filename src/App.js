2fa95483c61f16efb1e8d799af46d0bc9a7c03d6
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-03-20 22:24:53 +0300

    input componenti ve helper'ın kullanım örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
 import "./App.css";
+import { Input } from "./components/core-components/Input";
+import { formPropertyTypeConverter } from "./helper/core-helpers";
 
 function App() {
-	return <div className="App">SERDAR</div>;
+	const [formData, setFormData] = useState({
+		amount: 0,
+		name: "Serdar Şerifoğlu",
+	});
+
+	const handleChange = (prop) => (event) => {
+		setFormData({
+			...formData,
+			[prop]: formPropertyTypeConverter(
+				formData,
+				prop,
+				event.target.value
+			),
+		});
+	};
+	return (
+		<div className="App">
+			SERDAR
+			<Input
+				type="number"
+				onChange={handleChange("amount")}
+				value={formData.amount}
+			></Input>
+			<Input
+				type="text"
+				onChange={handleChange("name")}
+				value={formData.name}
+			></Input>
+		</div>
+	);
 }
 
 export default App;
